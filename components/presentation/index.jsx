d5f2339c5f0808071a0b3de90ea5b02e048b748b
Fathismael93 <fathismael@gmail.com>
2026-01-07 11:05:00 +0300

    Updated Presentation page

--- components/presentation/index.jsx
+++ components/presentation/index.jsx
@@ -103,13 +103,13 @@ const PresentationComponent = () => {
         ]}
       />
 
-      {/* SECTION 1: Parallax - height: calc(100vh - 6.25rem) */}
-      <section className="presentation-hero">
+      {/* SECTION 1: Parallax */}
+      <section className="first">
         <Parallax bgColor="#0c0c1d" title="Presentation" planets="/sun.png" />
       </section>
 
-      {/* SECTION 2: Carte SKY IS THE LIMIT - height: 100vh */}
-      <section className="presentation-main">
+      {/* SECTION 2: Carte SKY IS THE LIMIT */}
+      <section className="others">
         {/* ✅ BACKGROUNDS OPTIMISÉS avec Next.js Image */}
         <div className="planets-background-container">
           <Image
@@ -152,8 +152,8 @@ const PresentationComponent = () => {
         </div>
       </section>
 
-      {/* SECTION 3: Grille des catégories - min-height: 100vh */}
-      <section className="presentation-categories">
+      {/* SECTION 3: Grille des catégories */}
+      <section className="others">
         {/* Backgrounds réutilisés */}
         <div className="planets-background-container">
           <Image
