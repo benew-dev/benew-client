625dcc7b7e65b3d3e693b32b64c0c7b7d8bc0931
Fathismael93 <fathismael@gmail.com>
2026-01-07 10:30:26 +0300

    Updated Presentation page

--- components/presentation/index.jsx
+++ components/presentation/index.jsx
@@ -152,8 +152,8 @@ const PresentationComponent = () => {
         </div>
       </section>
 
-      {/* SECTION 3: Grille des catégories */}
-      <section className="others">
+      {/* SECTION 3: Grille des catégories - DIV au lieu de SECTION */}
+      <div className="categories-section">
         {/* Backgrounds réutilisés */}
         <div className="planets-background-container">
           <Image
@@ -198,7 +198,7 @@ const PresentationComponent = () => {
             ))}
           </div>
         </div>
-      </section>
+      </div>
 
       <PresentationModal
         isOpen={isModalOpen}
