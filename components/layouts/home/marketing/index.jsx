9778e010bdaaf22c5d2faf452e3b34291868d4dd
Fathismael93 <fathismael@gmail.com>
2026-03-18 18:04:29 +0300

    Restored old version of  marketing section

--- components/layouts/home/marketing/index.jsx
+++ components/layouts/home/marketing/index.jsx
@@ -7,7 +7,6 @@ import Link from 'next/link';
 const MarketingHome = () => {
   return (
     <div className="main-content">
-      {/* BLOC 1 - Tirelire */}
       <Image
         src="/tirelire.png"
         alt="Tirelire symbolisant l'économie et les profits"
@@ -18,45 +17,15 @@ const MarketingHome = () => {
         unoptimized
       />
 
-      {/* BLOC 2 - Titres + Bouton */}
       <div className="text-container">
         <h2 className="main-title">GÉNÈRES PLUS DE PROFIT,</h2>
         <h2 className="main-title">PAIES MOINS DE CHARGES</h2>
 
+        {/* ✅ CORRECTION : Link au lieu de button pour SEO */}
         <Link href="/blog" className="profit-blog-link">
           En savoir plus
         </Link>
       </div>
-
-      {/* BLOC 3 - Image croissance */}
-      <Image
-        src="/homepage/croissance.png"
-        alt="Graphique de croissance"
-        width={190}
-        height={190}
-        className="deco-image deco-croissance"
-        unoptimized
-      />
-
-      {/* BLOC 4 - Image île */}
-      <Image
-        src="/homepage/ile.png"
-        alt="Île tropicale"
-        width={190}
-        height={190}
-        className="deco-image deco-ile"
-        unoptimized
-      />
-
-      {/* BLOC 5 - Image globe */}
-      <Image
-        src="/homepage/globe_voyage.png"
-        alt="Globe voyage"
-        width={190}
-        height={190}
-        className="deco-image deco-globe"
-        unoptimized
-      />
     </div>
   );
 };
